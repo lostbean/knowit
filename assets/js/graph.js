ddad713ae299cdbd9dd780185e1236463417db1a
lostbean <talktoedgar@gmail.com>
2023-08-18 12:49:17 -0300

    load data from server

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -33,26 +33,33 @@ export const Graph = {
                 }
             ],
 
-            elements: {
-                nodes: [
-                    { data: { id: 'a', parent: 'b' }, position: { x: 215, y: 85 } },
-                    { data: { id: 'b' } },
-                    { data: { id: 'c', parent: 'b' }, position: { x: 300, y: 85 } },
-                    { data: { id: 'd' }, position: { x: 215, y: 175 } },
-                    { data: { id: 'e' } },
-                    { data: { id: 'f', parent: 'e' }, position: { x: 300, y: 175 } }
-                ],
-                edges: [
-                    { data: { id: 'ad', source: 'a', target: 'd' } },
-                    { data: { id: 'eb', source: 'e', target: 'b' } }
+            // elements: {
+            //     nodes: [
+            //         { data: { id: 'a', parent: 'b' }, position: { x: 215, y: 85 } },
+            //         { data: { id: 'b' } },
+            //         { data: { id: 'c', parent: 'b' }, position: { x: 300, y: 85 } },
+            //         { data: { id: 'd' }, position: { x: 215, y: 175 } },
+            //         { data: { id: 'e' } },
+            //         { data: { id: 'f', parent: 'e' }, position: { x: 300, y: 175 } }
+            //     ],
+            //     edges: [
+            //         { data: { id: 'ad', source: 'a', target: 'd' } },
+            //         { data: { id: 'eb', source: 'e', target: 'b' } }
 
-                ]
-            },
+            //     ]
+            // },
 
             layout: {
                 name: 'preset',
                 padding: 5
             }
+        });
+
+        this.handleEvent("add_points", ({ points }) => {
+            console.log(points);
+            cy.add(points);
+            cy.layout({ name: 'random', fit: true}).run();
+            cy.fit();
         });
 
     }
--- lib/knowit_web/live/graph_live.ex
+++ lib/knowit_web/live/graph_live.ex
@@ -1,5 +1,5 @@
 defmodule KnowitWeb.GraphLive do
-use KnowitWeb, :live_view
+  use KnowitWeb, :live_view
   @impl true
   def mount(_params, _session, socket) do
     schedule_update()
@@ -7,11 +7,26 @@ use KnowitWeb, :live_view
   end
 
   @impl true
-  def handle_event("next", _, socket) do
+  def handle_event("loadGraph", _, socket) do
     schedule_update()
-    {:noreply, socket |> push_event("points", %{points: get_points()})}
+    {:noreply, socket |> push_event("add_points", %{points: get_points()})}
   end
 
   defp schedule_update, do: self() |> Process.send_after(:update, 2000)
-  defp get_points, do: 1..7 |> Enum.map(fn _ -> :rand.uniform(100) end)
+
+  defp get_points,
+    do: %{
+      nodes: [
+        %{data: %{id: "a", parent: "b"}},
+        %{data: %{id: "b"}},
+        %{data: %{id: "c", parent: "b"}},
+        %{data: %{id: "d"}},
+        %{data: %{id: "e"}},
+        %{data: %{id: "f", parent: "e"}}
+      ],
+      edges: [
+        %{data: %{id: "ad", source: "a", target: "d"}},
+        %{data: %{id: "eb", source: "e", target: "b"}}
+      ]
+    }
 end
