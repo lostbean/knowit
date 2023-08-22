b78bfb101d7a07d9ff524daa2698fed6981fb902
lostbean <talktoedgar@gmail.com>
2023-08-22 11:07:00 -0300

    Use new CI env for HF caching

--- mix.exs
+++ mix.exs
@@ -55,7 +55,7 @@ defmodule Knowit.MixProject do
       {:exla, "~> 0.6"},
       {:openai, "~> 0.5.2"},
       # Discord Bot
-      {:nostrum, "~> 0.8.0", runtime: false}
+      {:nostrum, "~> 0.8.0", runtime: Mix.env() != :CI}
     ]
   end
 
