4fb11f88b6f5cf1322858887dfdbeb1feb57cc6d
lostbean <talktoedgar@gmail.com>
2023-09-03 08:54:13 -0300

    making migration retriable

--- priv/repo/migrations/20230901014010_create_graph_db.exs
+++ priv/repo/migrations/20230901014010_create_graph_db.exs
@@ -1,7 +1,20 @@
 defmodule Knowit.Repo.Migrations.CreateGraphDb do
   use Ecto.Migration
 
+  @graph_database "knowit_graph"
+
+  def has_entries(query) do
+    repo().query!(query).num_rows > 0
+  end
+
   def change do
-    execute "SELECT create_graph('knowit_graph');", ""
+    if has_entries(
+         "select * from pg_tables where (schemaname = 'ag_catalog') and (tablename = 'ag_graph');"
+       ) && has_entries(
+        "select name from ag_catalog.ag_graph where (name = '#{@graph_database}');") do
+      IO.puts("AGE graph has already been created.")
+    else
+      repo().query!("SELECT * FROM create_graph('#{@graph_database}');")
+    end
   end
 end
