167081865aed763516431daace7935393aca7369
lostbean <talktoedgar@gmail.com>
2023-12-27 14:13:09 -0300

    use flake

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "Dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, unstable, flake-utils, ... }:
+    let utils = flake-utils;
+    in utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        unstable_pkgs = unstable.legacyPackages.${system};
+      in {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs;
+            let
+              frameworks = darwin.apple_sdk.frameworks;
+              inherit (lib) optional optionals;
+            in [
+              # Dev environment
+              elixir
+              elixir_ls
+              nodejs
+              erlang
+              rebar3
+            ] ++ optionals stdenv.isDarwin [
+              # Dev environment
+              flyctl
+              postgresql
+              docker
+            ] ++ optionals stdenv.isLinux [
+              # Docker build
+              (python3.withPackages (ps: with ps; [ pip numpy ]))
+              stdenv
+              gcc
+              gnumake
+              bazel
+              glibc
+              gcc
+              glibcLocales
+            ] ++ optionals stdenv.isDarwin [
+              # add macOS headers to build mac_listener and ELXA
+              frameworks.CoreServices
+              frameworks.CoreFoundation
+              frameworks.Foundation
+            ];
+
+          shellHook = ''
+            printf '\u001b[32m
+            Know-it!
+            \e[0m
+            '
+          '';
+        };
+      });
+}
